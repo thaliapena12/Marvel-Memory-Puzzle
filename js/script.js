b4ba2417927c0fa14bdb8969fa1e961b0843bf7e
Thalia <thaliapena12@gmail.com>
2020-04-30 15:42:52 -0700

    add styling and overlay

--- js/script.js
+++ js/script.js
@@ -2,29 +2,62 @@
 class Game {
     constructor(cardsDeck){
         this.cardsArray = cardsDeck;
-        this.busy = true;
+       
 
     }
 
     start(){
         this.checkCard = null;
         this.matched = [];
-        this.hide = [];
+        this.busy = true;
+        setTimeout(() => {
+            this.shuffle();
+            this.busy = false;
+        }, 500);
+        this.hide();
 
     }
 
-    flip(){
+    flip(card){
+        if (this.checkFlip(card)) {
+            card.classList.add('visible');
+            if (this.checkCard)
+                this.cardComp(card);
+            else
+                this.checkCard = card;
+        }
 
     }
     hide(){
+        this.cardsArray.forEach(card => {
+            card.classList.remove('visible');
+            card.classList.remove('matched');
+        });
 
     }
-    cardComp(card1, card2){
+    checkMatch(card1, card2){
         this.matched.push(card1);
         this.matched.push(card2);
         card1.classList.add('matched');
         card2.classList.add('matched');
 
+
+    }
+    notMacth(card1, card2){
+        this.busy = true;
+        setTimeout(() => {
+            card1.classList.remove('visible');
+            card2.classList.remove('visible');
+            this.busy = false;
+        }, 1000);
+    }
+    cardComp(card){
+        if (this.cardType(card) === this.cardType(this.checkCard))
+            this.cardComp(card, this.checkCard);
+        else
+            this.notMatch(card, this.checkCard);
+
+        this.checkCard = null;
     }
     cardType(card) {
         return card.getElementsByClassName('value')[0].src;
@@ -40,6 +73,14 @@ class Game {
     checkFlip(card) {
         return !this.busy && !this.matched.includes(card) && card !== this.checkCard;
     }
+
+    gameOver(){
+        document.getElementById('game-over').classList.add('visible');
+    }
+
+    won(){
+        document.getElementById('won').classList.add('visible');
+    }
     
    
 
@@ -48,11 +89,13 @@ class Game {
 
 function gameReady(){
     cardsDeck = Array.from(document.getElementsByClassName('card'));
-
+    let game = new Game(cardsDeck);
+    game.start();
     cardsDeck.forEach(card => {
         console.log(card)
         card.addEventListener('click', () => {
-            //flip(card);
+           game.flip(card)
+        
         });
     });
 }
