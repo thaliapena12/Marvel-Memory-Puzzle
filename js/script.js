4ad996efaa463bcd435bf4a81286d7339a5592ab
Thalia <thaliapena12@gmail.com>
2020-04-30 18:11:55 -0700

    update script

--- js/script.js
+++ js/script.js
@@ -1,28 +1,47 @@
 
 class Game {
-    constructor(cardsDeck){
+    constructor(timeTotal, cardsDeck){
         this.cardsArray = cardsDeck;
-       
+        this.timeTotal = timeTotal;
+        this.timeLeft = timeTotal;
+        this.timer = document.getElementById('time-left');
+        this.ticker = document.getElementById('flips');
 
     }
 
     start(){
         this.checkCard = null;
         this.matched = [];
+        this.totalClicks = 0;
+        this.timeLeft = this.timeTotal;
         this.busy = true;
         setTimeout(() => {
             this.shuffle();
+            this.counter = this.startCounter();
             this.busy = false;
         }, 500);
         this.hide();
+        this.timer.innerText = this.timeLeft;
+        this.ticker.innerText = this.totalClicks;
 
+    }
+
+    startCounter(){
+        return setInterval(() => {
+            this.timeLeft--;
+            this.timer.innerText = this.timeLeft;
+            if (this.timeLeft === 0)
+                this.gameOver();
+        }, 1000);
     }
 
     flip(card){
         if (this.checkFlip(card)) {
             card.classList.add('visible');
+            this.totalClicks++;
+            this.ticker.innerText = this.totalClicks;
             if (this.checkCard)
-                this.cardComp(card);
+                this.checkMatch(card);
             else
                 this.checkCard = card;
         }
@@ -35,15 +54,16 @@ class Game {
         });
 
     }
-    checkMatch(card1, card2){
-        this.matched.push(card1);
-        this.matched.push(card2);
-        card1.classList.add('matched');
-        card2.classList.add('matched');
+    checkMatch(card){
+        if (this.cardType(card) === this.cardType(this.checkCard))
+            this.cardComp(card, this.checkCard);
+        else
+            this.notMatch(card, this.checkCard);
 
+        this.checkCard = null;
 
     }
-    notMacth(card1, card2){
+    notMatch(card1, card2){
         this.busy = true;
         setTimeout(() => {
             card1.classList.remove('visible');
@@ -51,13 +71,13 @@ class Game {
             this.busy = false;
         }, 1000);
     }
-    cardComp(card){
-        if (this.cardType(card) === this.cardType(this.checkCard))
-            this.cardComp(card, this.checkCard);
-        else
-            this.notMatch(card, this.checkCard);
-
-        this.checkCard = null;
+    cardComp(card1, card2){
+        this.matched.push(card1);
+        this.matched.push(card2);
+        card1.classList.add('matched');
+        card2.classList.add('matched');
+        if (this.matched.length === this.cardsArray.length)
+            this.victory();
     }
     cardType(card) {
         return card.getElementsByClassName('value')[0].src;
@@ -76,9 +96,11 @@ class Game {
 
     gameOver(){
         document.getElementById('game-over').classList.add('visible');
+        clearInterval(this.counter);
     }
 
     won(){
+        clearInterval(this.counter);
         document.getElementById('won').classList.add('visible');
     }
     
@@ -88,13 +110,22 @@ class Game {
 
 
 function gameReady(){
-    cardsDeck = Array.from(document.getElementsByClassName('card'));
-    let game = new Game(cardsDeck);
-    game.start();
+    let cardsDeck = Array.from(document.getElementsByClassName('card'));
+    let overlay = Array.from(document.getElementsByClassName('overlay'));
+    let game = new Game(100, cardsDeck);
+
+
+    overlay.forEach(over => {
+        // console.log(over)
+        over.addEventListener('click', () => {
+            over.classList.remove('visible');
+            game.start();
+        });
+    });
     cardsDeck.forEach(card => {
-        console.log(card)
+           // console.log(card)
         card.addEventListener('click', () => {
-           game.flip(card)
+           game.flip(card);
         
         });
     });
